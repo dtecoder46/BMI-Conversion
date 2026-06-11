75d735a4570c0d745e223cd77a5a94cca7ad3b26
Daniel Te <dte.biotech46@gmail.com>
2026-06-10 23:38:39 -0400

    Added BMI & height to weight conversion functionality

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ prompt = input(
     "Welcome to the BMI Converter! Do you want to convert to pounds or BMI?: ")
 
 if prompt == "pounds":
-    print("placeholder")
+    BMI = float(input("enter your BMI in kg/m^2: "))
+    height = float(input("enter your height in inches: "))
+
+    weight = (BMI/703) * (height**2)
+
+    print(f"Your weight is {weight} pounds")
+
 
 elif prompt == "BMI":
     # BMI
-    mass = int(input("enter your mass in pounds: "))
-    height = int(input("enter your height in inches: "))
+    mass = float(input("enter your mass in pounds: "))
+    height = float(input("enter your height in inches: "))
 
     BMI = (mass/height**2) * 703
 
