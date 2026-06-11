be98d174e1cecfa21fe60d4f485b086ffe209f78
Daniel Te <dte.biotech46@gmail.com>
2026-06-10 23:47:58 -0400

    Ensured invalid input would allow user to re-enter input

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if prompt == "pounds":
 
     print(f"Your weight is {weight} pounds")
 
-
 elif prompt == "BMI":
     # BMI
     mass = float(input("enter your mass in pounds: "))
@@ -21,3 +20,35 @@ elif prompt == "BMI":
 
 else:
     print("Invalid input, choose one of the two options")
+
+invalid = True
+
+while invalid == True:
+    prompt = input(
+    "Welcome to the BMI Converter! Do you want to convert to pounds or BMI?: ")
+
+    if prompt == "pounds":
+        BMI = float(input("enter your BMI in kg/m^2: "))
+        height = float(input("enter your height in inches: "))
+
+        weight = (BMI/703) * (height**2)
+
+        print(f"Your weight is {weight} pounds")
+
+        invalid = False
+
+    elif prompt == "BMI":
+        # BMI
+        mass = float(input("enter your mass in pounds: "))
+        height = float(input("enter your height in inches: "))
+
+        BMI = (mass/height**2) * 703
+
+        print(f"Your BMI is {BMI} kg/m^2")
+
+        invalid = False
+
+    else:
+        print("Invalid input, choose one of the two options")
+
+        invalid = True
